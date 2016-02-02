47aad7f956e058d0f83b11ebdbfb9c72f9be4e36
LFer <lferreira@datamatic.com.uy>
2016-02-02 14:33:31 -0300

    funcionando

---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
+{
+    'name': 'QR',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Felipe Ferreira',
+    'website': 'http://www.datamatic.com.uy',
+    'category': 'Testing',
+    'summary': '',
+    'description': """Tratando de generar codigos QR.
+    """,
+    'data': [
+        'views/qr_code_view.xml',
+    ],
+    'demo': [
+    ],
+    'css':[],
+    'auto_install': False,
+    'installable': True,
+    'application': True
+}
